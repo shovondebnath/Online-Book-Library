ecb42ad7f01772ec0b32d4c1c9540b93b4f3fc5e
shovondebnath <23201142@uap-bd.edu>
2026-05-08 00:42:07 +0600

    Update Django settings and URL configurations for enhanced functionality and security

--- core/core/urls.py
+++ core/core/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('authentication.urls')),
+    path('', include('book.urls')),
+    path('', include('app.urls')),
+    path('staff_page/', include('data_entry.urls')),
 ]
