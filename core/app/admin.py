5595e02780a7de4c411583a9b601f5cf4018b4a0
shovondebnath <23201142@uap-bd.edu>
2026-05-08 01:17:04 +0600

    Register all models in the Django admin interface for better management

--- core/app/admin.py
+++ core/app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import CreditWallet, CreditTransaction, Category, Book, MyBook, Borrow, ReadingProgress, Rating, Review, ReviewReaction
 
 # Register your models here.
+
+admin.site.register(CreditWallet)
+admin.site.register(CreditTransaction)
+admin.site.register(Category)
+admin.site.register(Book)
+admin.site.register(MyBook)
+admin.site.register(Borrow)
+admin.site.register(ReadingProgress)
+admin.site.register(Rating)
+admin.site.register(Review)
+admin.site.register(ReviewReaction)
